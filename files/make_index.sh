ec2be573a57933708ea4bec729a589fc097d2d61
M Hryc <cyhr@platinum.edu.pl>
2026-09-18 20:02:36 +0200

    working index

--- files/make_index.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-tree \
-    --gitignore \
-    -H "https://orski-hryc.github.io/files" \
-    -o index.html
---
+++ make_index.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+tree \
+    --gitignore \
+    -H "https://orski-hryc.github.io" \
+    -o files/index.html \
+    "files"
